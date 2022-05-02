704f4bc437b32146272744e0c2375394716d5a6f
jevontorres <jevontor@usc.edu>
2022-05-01 20:17:26 -0700

    sql

--- finalproj.sql
+++ finalproj.sql
@@ -16,7 +16,7 @@ CREATE TABLE ARTISTS (
 CREATE TABLE ALBUMS (
 album_id INT PRIMARY KEY,
 album_name VARCHAR(50),
-cover_url VARCHAR(50),
+cover_url VARCHAR(100),
 artist_id INT,
 FOREIGN KEY (artist_id) REFERENCES ARTISTS(artist_id),
 duration INT,
@@ -63,3 +63,7 @@ rating INT NOT NULL,
 user_id INT NOT NULL,
 FOREIGN KEY (user_id) REFERENCES USERS(user_id)
 );
+
+INSERT INTO `finalproj`.`users` (`user_id`, `username`, `email`, `pass`) VALUES ('1', 'guest', 'guest@mail.com', 'guest');
+INSERT INTO `finalproj`.`artists` (`artist_id`, `artist_name`) VALUES ('1', 'Olivia Rodrigo');
+INSERT INTO `finalproj`.`albums` (`album_id`, `album_name`, `cover_url`, `artist_id`, `duration`, `releasedate`) VALUES ('1', 'Sour', 'https://upload.wikimedia.org/wikipedia/en/b/b2/Olivia_Rodrigo_-_SOUR.png', '1', '2081', '05/21/21');
